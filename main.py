415263ed1c22282806631b20ee3b2d3bd56b98c0
shan01dian <119046982+shan01dian@users.noreply.github.com>
2024-10-14 15:49:00 +0800

    Add files via upload
    Basic information of developer Chen Jianyuan, including student number, name and class

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+def Student_information():
+    name = "陈建源"
+    student_id = "24516442"
+    class_name = "G223"
+
+    print(f"name: {name}")
+    print(f"student id: {student_id}")
+    print(f"class name: {class_name}")
+
+
+if __name__ == "__main__":
+    Student_information()
